89a0ceaf1fbc7699b6c7e95a6fb1f9439dab4e5c
Debomoy Patra <debomoy.titu@gmail.com>
2026-06-25 04:59:11 +0530

    ollaaa

--- light_curve.py
+++ light_curve.py
@@ -2,8 +2,9 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 from lightkurve import search_lightcurve
+import numpy as np
 
-koi = pd.read_csv("cumulative_2026.06.24_09.53.31.csv" , comment='#')
+koi = pd.read_csv("koi_cumulative_labeled.csv" , comment='#')
 
 subset=(
     koi.groupby("signal_class", group_keys=False).head(100)
@@ -12,7 +13,25 @@ os.makedirs("dataset", exist_ok=True)
 
 records=[]
 
-for row in subset.itterows():
+for row in subset.iterrows():
     kepid = row["kepid"]
     label = row["signal_class"]
     lc = search_lightcurve(f"KIC {kepid}", mission="kepler").download()
+    lc = lc.remove_nans()
+    lc = lc.normalize()
+
+    np.savez(
+        f"dataset/{kepid}.npz",
+        time=lc.time.value,
+        flux=lc.flux.value
+        )
+
+    records.append({
+        "kepid": kepid,
+        "label": label,
+        "file": f"dataset/{kepid}.npz"
+        })
+
+    print(f"Downloaded {kepid}")
+
+pd.DataFrame(records).to_csv("dataset_index.csv", index=False)
