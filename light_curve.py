50807afccb64058fb076051f831cd1d7ec2080ab
Debomoy Patra <debomoy.titu@gmail.com>
2026-06-25 03:57:04 +0530

    Dataset fully loaded

--- light_curve.py
+++ light_curve.py
@@ -1,15 +1,20 @@
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
-koi = pd.read_csv("cumulative_2026.06.24_09.53.31.csv" , comment='#')
-sample=koi.iloc[0]
-print(sample["kepid"])
-
 from lightkurve import search_lightcurve
 
-lc= search_lightcurve(
-    "KIC 10797460",
-    mission ="Kepler"
-).download()
-lc.plot()
-plt.show()
+koi = pd.read_csv("cumulative_2026.06.24_09.53.31.csv" , comment='#')
+
+output_dir = "plots"
+os.makedirs(output_dir, exist_ok=True)
+
+for kepid in koi["kepid"]:
+
+    lc= search_lightcurve(
+        f"KIC {sample("kepid")}",
+        mission ="Kepler"
+        ).download()
+    lc.plot()
+    plt.savefig(os.path.join(output_dir, f"KIC_{kepid}.png"))
+    plt.close()
 
