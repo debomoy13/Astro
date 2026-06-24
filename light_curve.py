097fb11adf1ee7b62bc4a047e1023a77dba96a93
Debomoy Patra <debomoy.titu@gmail.com>
2026-06-25 04:40:57 +0530

    ollaaa

--- light_curve.py
+++ light_curve.py
@@ -8,7 +8,11 @@ koi = pd.read_csv("cumulative_2026.06.24_09.53.31.csv" , comment='#')
 subset=(
     koi.groupby("signal_class", group_keys=False).head(100)
 )
-os.makedirs("dataset", exit_ok=True)
+os.makedirs("dataset", exist_ok=True)
 
 records=[]
 
+for row in subset.itterows():
+    kepid = row["kepid"]
+    label = row["signal_class"]
+    lc = search_lightcurve(f"KIC {kepid}", mission="kepler").download()
