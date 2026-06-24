7a08b6804d6e3cdfe0a6eac40b749d1553fc1039
Debomoy Patra <debomoy.titu@gmail.com>
2026-06-25 01:54:01 +0530

    got the plots

--- light_curve.py
+++ light_curve.py
@@ -1,4 +1,15 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 koi = pd.read_csv("cumulative_2026.06.24_09.53.31.csv" , comment='#')
 sample=koi.iloc[0]
 print(sample["kepid"])
+
+from lightkurve import search_lightcurve
+
+lc= search_lightcurve(
+    "KIC 10797460",
+    mission ="Kepler"
+).download()
+lc.plot()
+plt.show()
+
