31c1fbc480765907d82761e11d2dc08ee9e8e8b3
Debomoy Patra <debomoy.titu@gmail.com>
2026-06-25 19:08:52 +0530

    data normalized

--- light_curve.py
+++ light_curve.py
@@ -1,10 +1,11 @@
+#this is a one time runner code , repeated running will cause repeated downloads
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
 from lightkurve import search_lightcurve
 import numpy as np
 
-koi = pd.read_csv("koi_cumulative_labeled.csv" , comment='#')
+koi = pd.read_csv("modified datasets/koi_cumulative_labeled.csv" , comment='#')
 
 subset=(
     koi.groupby("signal_class", group_keys=False).head(10)
